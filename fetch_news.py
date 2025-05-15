8e827520e0bd942540de74dac5ee0e0a64d5f50c
swoodard02 <66227318+swoodard02@users.noreply.github.com>
2025-05-15 18:53:04 -0400

    Update fetch_news.py

--- fetch_news.py
+++ fetch_news.py
@@ -5,7 +5,7 @@ import datetime
 def get_high_impact_events():
     feed_url = "https://www.myfxbook.com/rss/forex-economic-calendar-events"
     feed = feedparser.parse(feed_url)
-    events = []
+    timestamps = []
     for entry in feed.entries:
         if '<span class="sprite sprite-common sprite-high-impact">' in entry.summary:
             try:
@@ -13,10 +13,11 @@ def get_high_impact_events():
             except ValueError:
                 dt = datetime.datetime.strptime(entry.published, "%a, %d %b %Y %H:%M:%S %Z")
             unix_ts = int(dt.timestamp())
-            events.append(unix_ts)
-    return events
+            timestamps.append(unix_ts)
+    return timestamps
 
-events = get_high_impact_events()
+timestamps = get_high_impact_events()
 
 with open("high_impact_news.json", "w") as f:
-    json.dump(events, f, indent=2)
+    json.dump(timestamps, f, indent=2)
+
